e02da5d08bb039bc9ce228d76aa364ebda0494c9
Brad Fitzpatrick <brad@danga.com>
2012-04-18 19:03:40 -0700

    kinda work

--- websomtep.go
+++ websomtep.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
+	"sync"
 
 	"code.google.com/p/go-smtpd/smtpd"
 	"code.google.com/p/go.net/websocket"
@@ -16,40 +15,112 @@ var (
 	wsAddr = flag.String("ws", "websomtep.danga.com", "websocket host[:port]")
 )
 
-type env struct {
-	*smtpd.BasicEnvelope
-}
-
-func (e *env) AddRecipient(rcpt smtpd.MailAddress) error {
-	if strings.HasPrefix(rcpt.Email(), "bad@") {
-		return errors.New("we don't send email to bad@")
-	}
-	return e.BasicEnvelope.AddRecipient(rcpt)
-}
 
 func onNewMail(c smtpd.Connection, from smtpd.MailAddress) (smtpd.Envelope, error) {
 	log.Printf("ajas: new mail from %q", from)
-	return &env{new(smtpd.BasicEnvelope)}, nil
+	for _, c := range clients() {
+		select {
+		case c <- &Message{From: from.Email()}:
+		default:
+		}
+	}
+	e := &Message{
+		From: from.Email(),
+	}
+	return e, nil
+}
+
+type Message struct {
+	From, To string
+	Subject  string
+	Body     string
+}
+
+func (m *Message) AddRecipient(rcpt smtpd.MailAddress) error {
+	m.To = rcpt.Email()
+	return nil
+}
+
+func (m *Message) BeginData() error { return nil }
+
+func (m *Message) Write(line []byte) error {
+	m.Body += string(line) + "\n"
+	return nil
+}
+
+func (m *Message) Close() error {
+	for _, c := range clients() {
+		select {
+		case c <- m:
+		default:
+		}
+	}
+	return nil
+}
+
+var (
+	mu sync.Mutex // guards clients
+	clientMap = map[chan *Message]bool{}
+)
+
+func register(c chan *Message) {
+	mu.Lock()
+	defer mu.Unlock()
+	clientMap[c] = true
+}
+
+func unregister(c chan *Message) {
+	mu.Lock()
+	defer mu.Unlock()
+	delete(clientMap, c)
+}
+
+func clients() (cs []chan *Message) {
+	mu.Lock()
+        defer mu.Unlock()
+	for c := range clientMap {
+		cs = append(cs, c)
+	}
+	return
 }
 
 func streamMail(ws *websocket.Conn) {
 	log.Printf("websocket connection from %v", ws.RemoteAddr())
-	type T struct {
-		Foo string
-		Bar string
-	}
-	websocket.JSON.Send(ws, &T{
-		Foo: "123",
-		Bar: "xyz",
+	msgc := make(chan *Message, 100)
+	register(msgc)
+	defer unregister(msgc)
+
+	deadc := make(chan bool, 2)
+
+	websocket.JSON.Send(ws, &Message{
+		Subject: "mail will appear appear",
 	})
-	for {
-		var msg T
-		err := websocket.JSON.Receive(ws, &msg)
-		if err != nil {
-			log.Printf("error reading from %v: %v", ws.RemoteAddr(), err)
-			return
+
+	// Wait for incoming messages. Don't really care about them, but
+	// use this to find out if client goes away.
+	go func() {
+		var msg Message
+		for {
+			err := websocket.JSON.Receive(ws, &msg)
+			if err != nil {
+				log.Printf("Receive error from %v: %v", ws.RemoteAddr(), err)
+				deadc <- true
+				return
+			}
+			log.Printf("Got message from %v: %+v", ws.RemoteAddr(), msg)
 		}
-		log.Printf("Got message from %v: %+v", ws.RemoteAddr(), msg)
+	}()
+
+	for {
+		select {
+		case <-deadc:
+			return
+		case m := <-msgc:
+			err := websocket.JSON.Send(ws, m)
+			if err != nil {
+				return
+			}
+		}
 	}
 }
 
